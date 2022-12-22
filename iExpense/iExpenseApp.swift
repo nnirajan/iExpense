008ea599e2c26ea83a9499b8d7f08e0db90aea78
Nirajan Shrestha <nirajan.shrestha@ebpearls.com>
2022-12-22 11:40:15 +0545

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by ebpearls on 22/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
